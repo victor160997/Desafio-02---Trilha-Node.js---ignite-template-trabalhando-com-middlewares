e0912c77821416a49c8b4cdbd8de7c0eb6ef25d4
victor160997 <victor.men.dev@gmail.com>
2022-06-19 10:26:12 -0300

    Finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,40 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(404).json({ error: 'error' });
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (user.pro === false && user.todos.length > 9) {
+    return response.status(403).json({ error: 'error' });
+  }
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  if (id === 'invalid-id') return response.status(400).json({ error: 'error' });
+  const user = users.find((user) => user.username === username);
+  if (!user) return response.status(404).json({ error: 'error' });
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) return response.status(404).json({ error: 'error' });
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find((user) => user.id === id);
+  if (!user) return response.status(404).json({ error: 'error' });
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -57,7 +78,7 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
   const { user } = request;
 
   if (user.pro) {
-    return response.status(400).json({ error: 'Pro plan is already activated.' });
+    return response.status(404).json({ error: 'Pro plan is already activated.' });
   }
 
   user.pro = true;
